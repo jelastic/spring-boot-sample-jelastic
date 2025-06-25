2f69196afc6ae2ea4a43a55ad2e9c5c9f5f44751
Dmytro Tsurko <22764205+dimkadt@users.noreply.github.com>
2025-06-25 12:20:14 +0300

    JE-71170 [Spring Boot Cluster]: 500 Internal Server Error for jar test project after deploy (#1)

--- src/main/java/sample/web/ui/Message.java
+++ src/main/java/sample/web/ui/Message.java
@@ -2,17 +2,16 @@ package sample.web.ui;
 
 import java.util.Calendar;
 
-import org.hibernate.validator.constraints.NotEmpty;
-
+import jakarta.validation.constraints.NotBlank;
 
 public class Message {
 
 	private Long id;
 
-	@NotEmpty(message = "Message is required.")
+	@NotBlank(message = "Message is required.")
 	private String text;
 
-	@NotEmpty(message = "Summary is required.")
+	@NotBlank(message = "Summary is required.")
 	private String summary;
 
 	private Calendar created = Calendar.getInstance();
